5552ceac33e3ed2fa397272c9e5a6ec0935fba0a
Arum <arum.galadima@gmail.com>
2018-01-14 10:04:26 -0500

    express
    building express view

--- personalsite/app.js
+++ personalsite/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
-var app = express();
+const app = express();
+const bodyParser = require('body-parser');
 const scrapeIt = require('scrape-it');
 const NBA = require('nba');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//use pug
+app.set('view engine', 'pug');
+
+//use css
+app.use('/static', express.static('css'));
 
 let url = "http://www.nba.com/players/lebron/james/";
 
@@ -20,7 +30,7 @@ console.log(curry.firstName);
 //when the search button on index.html is pressed, it should send the data to the app.js file 
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World?')
 })
  
 app.listen(3000)
