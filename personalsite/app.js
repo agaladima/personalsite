b19439046eb832c2f6a6e2928bb4bc3fad1c3904
Arum <arum.galadima@gmail.com>
2018-01-24 23:52:59 -0500

    still trying to scrape

--- personalsite/app.js
+++ personalsite/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const NBA = require('nba');
-const upload = multer();
+const cheerio = require('cheerio');
 const routes = require('./routes');
 const url = 'http://www.nba.com/players';
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -16,14 +17,98 @@ app.use('/static', express.static('css'));
 
 app.use('/', routes);
 
-//scrape nba page for info
-let first = 'lebron';
-let last = 'james';
-scrapeIt(url+'/'+first+'/'+last, {
-	playerInfo: {
-		name: '.full_table:nth-child(2)'
-	}
+
+app.get('/', function(req, res){
+
+    const urlP = 'http://www.nba.com/players/';
+   	let first = 'lebron';
+   	let last = 'james';
+   	let url = urlP + first + '/' + last; 
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            let $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            let fname, lname, name, height, weight, number, position, team, ppg, apg, rpg, mpg, bpg;
+            let json = { playerInfo : {
+            	name: '',
+            	height: '',
+            	weight: '',         	
+            	number: '',
+            	position: '',
+            	team: '',
+            },
+            playerStats : {
+            	ppg: '',
+            	apg: '',
+            	rpg: '',
+            	mpg: '',
+            	bpg: ''
+            }};
+
+            //get elements
+            fname = $('.nba-player-header__first-name').text();
+            lname = $('.nba-player-header__last-name').text();
+            name = fname + ' ' + lname;
+            json.playerInfo.name = name;
+
+            let height1 = $('.nba-player-vitals__top-info-imperial').children().first().text();
+            let height2 = $('.nba-player-vitals__top-info-imperial:nth-child(2)').text();;
+            height = height1 + "ft" + height2 + "in";
+            json.playerInfo.height = height;
+
+            weight = $('.nba-player-vitals__top-info-imperial:nth-child(3)').text();
+            json.playerInfo.weight = weight;
+
+            number = $('.nba-player-header__jersey-number').text();
+            json.playerInfo.number = number;
+
+            position = $('.nba-player-header__position').text();
+            json.playerInfo.position = position;
+
+            team = $('.nba-detail-header__team-logo').text();
+            json.playerInfo.team = team;
+
+            ppg = $('.nba-player-season-career-stats table tbody:nth-child(6)');
+            json.playerStats.ppg = ppg;
+
+            apg = $('.nba-player-season-career-stats table tbody:nth-child(8)');
+            json.playerStats.apg = apg;
+
+            rpg = $('.nba-player-season-career-stats table tbody:nth-child(7)');
+            json.playerStats.rpg = rpg;
+
+            mpg = $('.nba-player-season-career-stats table tbody:nth-child(2)');
+            json.playerStats.mpg = mpg;
+
+            bpg = $('.nba-player-season-career-stats table tbody:nth-child(9)');
+            json.playerStats.bpg = bpg;
+        }
+
+    })
+    // To write to the system we will use the built in 'fs' library.
+	// In this example we will pass 3 parameters to the writeFile function
+	// Parameter 1 :  output.json - this is what the created filename will be called
+	// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+	// Parameter 3 :  callback function - a callback function to let us know the status of our function
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+	    console.log('File successfully written! - Check your project directory for the output.json file');
+	});
 });
+
+
+
+
 
 
 
@@ -64,3 +149,6 @@ app.use(function(error, req, res, next) {
 app.listen(3000, () => {
 	console.log('The app is running on local host:3000.');
 });
+
+exports = module.exports = app;
+
--- personalsite/scraper.js
+++ personalsite/scraper.js
@@ -2,15 +2,14 @@ const express = require('express');
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
-const app     = express();
+const app = express();
 
-app.get('/scrape', function(req, res){
-    // The URL we will scrape from - in our example Anchorman 2.
+app.get('/scraper', function(req, res){
 
     const urlP = 'http://www.nba.com/players/';
-   	let first = req.query.firstname;
-   	let last = req.query.lastname;
-   	let url = urlP + first + '/' + last; 
+    let first = 'lebron';
+    let last = 'james';
+    let url = 'http://www.nba.com/players/lebron/james'; 
     // The structure of our request call
     // The first parameter is our URL
     // The callback function takes 3 parameters, an error, response status code and the html
@@ -28,19 +27,19 @@ app.get('/scrape', function(req, res){
 
             let fname, lname, name, height, weight, number, position, team, ppg, apg, rpg, mpg, bpg;
             let json = { playerInfo : {
-            	name: ,
-            	height: ,
-            	weight: ,            	
-            	number: ,
-            	position: ,
-            	team: ,
+                name: '',
+                height: '',
+                weight: '',             
+                number: '',
+                position: '',
+                team: '',
             },
             playerStats : {
-            	ppg: ,
-            	apg: ,
-            	rpg: ,
-            	mpg: ,
-            	bpg: 
+                ppg: '',
+                apg: '',
+                rpg: '',
+                mpg: '',
+                bpg: ''
             }};
 
             //get elements
@@ -51,10 +50,10 @@ app.get('/scrape', function(req, res){
 
             let height1 = $('.nba-player-vitals__top-info-imperial').children().first().text();
             let height2 = $('.nba-player-vitals__top-info-imperial:nth-child(2)').text();;
-            height = height1 + "'" + height2"''";
+            height = height1 + "ft" + height2 + "in";
             json.playerInfo.height = height;
 
-            let weight = $('.nba-player-vitals__top-info-imperial:nth-child(3)').text();
+            weight = $('.nba-player-vitals__top-info-imperial:nth-child(3)').text();
             json.playerInfo.weight = weight;
 
             number = $('.nba-player-header__jersey-number').text();
@@ -66,35 +65,36 @@ app.get('/scrape', function(req, res){
             team = $('.nba-detail-header__team-logo').text();
             json.playerInfo.team = team;
 
-            ppg = $('.nba-player-season-career-stats table tbody:nth-child(6)');
+            ppg = $('.nba-player-season-career-stats table tbody:nth-child(6)').text();
             json.playerStats.ppg = ppg;
 
-            apg = $('.nba-player-season-career-stats table tbody:nth-child(8)');
+            apg = $('.nba-player-season-career-stats table tbody:nth-child(8)').text();
             json.playerStats.apg = apg;
 
-            rpg = $('.nba-player-season-career-stats table tbody:nth-child(7)');
+            rpg = $('.nba-player-season-career-stats table tbody:nth-child(7)').text();
             json.playerStats.rpg = rpg;
 
-            mpg = $('.nba-player-season-career-stats table tbody:nth-child(2)');
+            mpg = $('.nba-player-season-career-stats table tbody:nth-child(2)').text();
             json.playerStats.mpg = mpg;
 
-            bpg = $('.nba-player-season-career-stats table tbody:nth-child(9)');
+            bpg = $('.nba-player-season-career-stats table tbody:nth-child(9)').text();
             json.playerStats.bpg = bpg;
         }
 
     })
     // To write to the system we will use the built in 'fs' library.
-	// In this example we will pass 3 parameters to the writeFile function
-	// Parameter 1 :  output.json - this is what the created filename will be called
-	// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-	// Parameter 3 :  callback function - a callback function to let us know the status of our function
+    // In this example we will pass 3 parameters to the writeFile function
+    // Parameter 1 :  output.json - this is what the created filename will be called
+    // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+    // Parameter 3 :  callback function - a callback function to let us know the status of our function
     fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-	    console.log('File successfully written! - Check your project directory for the output.json file');
-	})
-})
+        console.log('File successfully written! - Check your project directory for the output.json file');
+    });
+});
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
-exports = module.exports = scraper;
+exports = module.exports = app;
 
-//https://scotch.io/tutorials/scraping-the-web-with-node-js
+
+
