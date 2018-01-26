e3e1f92f58ce392846d6bc9b4943381c459988a9
Arum <arum.galadima@gmail.com>
2018-01-26 00:20:45 -0500

    trying to render json

--- personalsite/app.js
+++ personalsite/app.js
@@ -19,91 +19,90 @@ app.use('/', routes);
 
 
 app.get('/', function(req, res){
+  const urlP = 'http://www.nba.com/players/';
+  let first = 'lebron';
+  let last = 'james';
+  let url = 'http://www.nba.com/players/lebron/james'; 
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 parameters, an error, response status code and the html
 
-    const urlP = 'http://www.nba.com/players/';
-   	let first = 'lebron';
-   	let last = 'james';
-   	let url = urlP + first + '/' + last; 
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
+  request(url, function(error, response, html){
+    // First we'll check to make sure no errors occurred when making the request
+    let json = { playerInfo: {
+    								name: '',
+    								pheight: '',
+		                weight: '',             
+		                number: '',
+		                position: '',
+		                team: '',
+    						},
+    						playerStats: {
+    							ppg: '',
+	                apg: '',
+	                rpg: '',
+	                mpg: '',
+	                bpg: ''										
+    						}
+    					};
+    if(!error){
+      // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+      
+      let $ = cheerio.load(html);
 
-    request(url, function(error, response, html){
+      // Finally, we'll define the variables we're going to capture
 
-        // First we'll check to make sure no errors occurred when making the request
-
-        if(!error){
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
-            let $ = cheerio.load(html);
-
-            // Finally, we'll define the variables we're going to capture
-
-            let fname, lname, name, height, weight, number, position, team, ppg, apg, rpg, mpg, bpg;
-            let json = { playerInfo : {
-            	name: '',
-            	height: '',
-            	weight: '',         	
-            	number: '',
-            	position: '',
-            	team: '',
-            },
-            playerStats : {
-            	ppg: '',
-            	apg: '',
-            	rpg: '',
-            	mpg: '',
-            	bpg: ''
-            }};
-
-            //get elements
-            fname = $('.nba-player-header__first-name').text();
-            lname = $('.nba-player-header__last-name').text();
-            name = fname + ' ' + lname;
-            json.playerInfo.name = name;
-
-            let height1 = $('.nba-player-vitals__top-info-imperial').children().first().text();
-            let height2 = $('.nba-player-vitals__top-info-imperial:nth-child(2)').text();;
-            height = height1 + "ft" + height2 + "in";
-            json.playerInfo.height = height;
-
-            weight = $('.nba-player-vitals__top-info-imperial:nth-child(3)').text();
-            json.playerInfo.weight = weight;
-
-            number = $('.nba-player-header__jersey-number').text();
-            json.playerInfo.number = number;
-
-            position = $('.nba-player-header__position').text();
-            json.playerInfo.position = position;
-
-            team = $('.nba-detail-header__team-logo').text();
-            json.playerInfo.team = team;
-
-            ppg = $('.nba-player-season-career-stats table tbody:nth-child(6)');
-            json.playerStats.ppg = ppg;
-
-            apg = $('.nba-player-season-career-stats table tbody:nth-child(8)');
-            json.playerStats.apg = apg;
-
-            rpg = $('.nba-player-season-career-stats table tbody:nth-child(7)');
-            json.playerStats.rpg = rpg;
-
-            mpg = $('.nba-player-season-career-stats table tbody:nth-child(2)');
-            json.playerStats.mpg = mpg;
-
-            bpg = $('.nba-player-season-career-stats table tbody:nth-child(9)');
-            json.playerStats.bpg = bpg;
-        }
-
-    })
-    // To write to the system we will use the built in 'fs' library.
-	// In this example we will pass 3 parameters to the writeFile function
-	// Parameter 1 :  output.json - this is what the created filename will be called
-	// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-	// Parameter 3 :  callback function - a callback function to let us know the status of our function
+      let fname, lname, height, weight, number, position, team, ppg, apg, rpg, mpg, bpg;
+      
+      //get player name
+      $('.nba-player-header__details-bottom').filter(function(){
+  			let data = $(this);
+      	fname = data.children().first().text().trim();
+      	lname = data.children().last().text().trim();
+      	let fullname = fname + ' ' + lname;
+      	json.playerInfo.name = fullname;
+    	});
+      $('.nba-player-header__details-top').filter(function(){
+  			let data = $(this);
+      	team = data.children().first().text().trim();
+      	json.playerInfo.team = team;
+      	number = data.children('.nba-player-header__jersey-number').text().trim();
+      	json.playerInfo.number = number;
+      	position = data.children('.nba-player-header__position').text().trim();
+      	json.playerInfo.position = position;
+    	});
+      $('.nba-player-vitals__top-left').filter(function() {
+        let data = $(this);
+        height = data.children('.nba-player-vitals__top-info-imperial').first().text().trim();
+        json.playerInfo.pheight = height;
+      });
+      $('.nba-player-vitals__top-right').filter(function() {
+        let data = $(this);
+        weight = data.children('.nba-player-vitals__top-info-imperial').first().text().trim();
+        json.playerInfo.weight = weight;
+      });
+      //get player stats
+      $('.nba-player-season-career-stats').children().children().last().children().filter(function() {
+        let data = $(this);
+        ppg = data.children('td:nth-child(6)').text().trim();
+        json.playerStats.ppg = ppg;
+        apg = data.children('td:nth-child(8)').text().trim();
+        json.playerStats.apg = apg;
+        rpg = data.children('td:nth-child(7)').text().trim();
+        json.playerStats.rpg = rpg;
+        mpg = data.children('td:nth-child(2)').text().trim();
+        json.playerStats.mpg = mpg;
+        bpg = data.children('td:nth-child(9)').text().trim();
+        json.playerStats.bpg = bpg;
+      });
+    }
     fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-	    console.log('File successfully written! - Check your project directory for the output.json file');
-	});
+      console.log('File successfully written! - Check your project directory for the output.json file');
+  	});
+  	res.render('/', {
+  		pName: json.playerInfo.name
+  	});
+  });
 });
 
 
